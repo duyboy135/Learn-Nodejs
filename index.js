7d92e3b4bebf64a484d37bb4ed9446378c8de201
duyboy135 <duyboy135@gmail.com>
2017-06-29 17:17:57 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var main = require('http');
 var Url = require('url');
 var outp = require('fs');
-app.set('port', (process.env.PORT || 5000));
 	
 main.createServer(function(req,res){
     res.writeHead(200, {'Content-Type': 'text/html'});
@@ -26,4 +25,4 @@ main.createServer(function(req,res){
     }
     res.end();
 
-}).listen(app.get('port'));
+}).listen((process.env.PORT || 5000));
